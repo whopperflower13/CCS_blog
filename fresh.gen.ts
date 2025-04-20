0186bd43359a8d9324af77a488abd1879e7982d6
whopperflower13 <namiponnusamasama@gmail.com>
2025-04-20 12:27:18 +1000

    ffffffffffffffffff

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -7,7 +7,6 @@ import * as $0 from "./routes/[slug].tsx";
 import * as $1 from "./routes/_404.tsx";
 import * as $2 from "./routes/feed.ts";
 import * as $3 from "./routes/index.tsx";
-import * as $4 from "./routes/study-threejs.tsx";
 
 const manifest = {
   routes: {
@@ -15,7 +14,6 @@ const manifest = {
     "./routes/_404.tsx": $1,
     "./routes/feed.ts": $2,
     "./routes/index.tsx": $3,
-    "./routes/study-threejs.tsx": $4,
   },
   islands: {},
   baseUrl: import.meta.url,
