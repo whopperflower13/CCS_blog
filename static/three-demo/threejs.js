89f8bea2b761fdc9def71217411c765071bc0e8f
whopperflower13 <142776447+whopperflower13@users.noreply.github.com>
2025-04-29 18:35:12 +1000

    hhh

--- static/three-demo/threejs.js
+++ static/three-demo/threejs.js
@@ -27,7 +27,8 @@ function animate() {
   requestAnimationFrame(animate);
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
-  renderer.render(scene, camera);
+  
 }
+renderer.render(scene, camera);
 
 animate();
