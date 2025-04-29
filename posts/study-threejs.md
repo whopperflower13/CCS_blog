69563a490482b34f8ced4c839ef4f38e81e142d8
whopperflower13 <142776447+whopperflower13@users.noreply.github.com>
2025-04-29 17:45:13 +1000

    ?

--- posts/study-threejs.md
+++ posts/study-threejs.md
@@ -10,4 +10,3 @@ I was finally fiiinally able to embed three.js!!
 <iframe src="/three-demo/index.html" width="100%" height="500" style="border: none;"></iframe>
 
 I iframed the index.html in this post:
-<code> <iframe src="/three-demo/index.html" width="100%" height="500" style="border: none;"></iframe> </code>
--- static/three-demo/threejs.js
+++ static/three-demo/threejs.js
@@ -1,29 +1,102 @@
 import * as THREE from "https://esm.sh/three@0.150.1";
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
+import { OrbitControls } from '250408/scripts/OrbitControls.js'
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+// Scene
+const scene = new THREE.Scene()
 
-camera.position.z = 5;
+// Object
+const geometry = new THREE.BufferGeometry()
 
-function animate() {
-  requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  renderer.render(scene, camera);
+const count = 500
+const positionsArray = new Float32Array(count * 3 * 3) //each triangle consists of 3 verticies which each contain 3 values
+
+for(let i = 0; i < count * 3 * 3; i++)
+{
+    positionsArray[i] = (Math.random() - 0.5) * 4
 }
 
-animate();
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
+geometry.setAttribute('position', positionsAttribute)
+
+// const positionsArray = new Float32Array ([
+//   //x,y,z values for three verticies
+//     0, 0, 0,
+//     0, 1, 0,
+//     1, 0, 0
+// ])
+
+// //create three.js buffer attribute
+// const positionsAttribute = new THREE.BufferAttribute (positionsArray, 3)
+
+
+// geometry.setAttribute('position', positionsAttribute)
+
+const material = new THREE.MeshBasicMaterial({ 
+    color: 'pink', 
+    wireframe: true
+})
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Sizes
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 3
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+// Animate
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
