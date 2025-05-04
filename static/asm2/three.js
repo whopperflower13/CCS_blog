3143b9cf3a5856be8c6b96b843b7c93ba442fe76
whopperflower13 <namiponnusamasama@gmail.com>
2025-05-04 20:04:38 +1000

    go

---
+++ static/asm2/three.js
@@ -0,0 +1,4 @@
+import * as THREE from "https://esm.sh/three@0.150.1";
+import { OrbitControls } from '../../node_modules/three/examples/jsm/controls/OrbitControls.js'
+console.log(THREE)
+console.log(OrbitControls)
